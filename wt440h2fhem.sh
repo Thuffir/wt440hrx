0c84e5e6bedd509ac1b15f29dfa7729f45bc702d
Gergely Budai <gbudai76@gmail.com>
2015-05-24 22:14:06 +0200

    status for <=14%

--- wt440h2fhem.sh
+++ wt440h2fhem.sh
@@ -6,9 +6,13 @@ PORT=7072
 while read housecode channel status battery humid temp; do
   batt_txt="ok"
   batt_stat=""
+  hum_prefix=""
   if [ $battery -eq 1 ]; then
     batt_txt="low"
     batt_stat=", Bat.low!"
   fi
-  echo -ne "setreading wt440h_${housecode}_${channel} temperature ${temp}\nsetreading wt440h_${housecode}_${channel} humidity ${humid}\nsetreading wt440h_${housecode}_${channel} battery ${batt_txt}\nset wt440h_${housecode}_${channel} ${temp}°C, ${humid}%${batt_stat}\n" | nc ${HOST} ${PORT}
+  if [ $humid -le 14 ]; then
+    hum_prefix="<="
+  fi
+  echo -ne "setreading wt440h_${housecode}_${channel} temperature ${temp}\nsetreading wt440h_${housecode}_${channel} humidity ${humid}\nsetreading wt440h_${housecode}_${channel} battery ${batt_txt}\nset wt440h_${housecode}_${channel} ${temp}°C, ${hum_prefix}${humid}%${batt_stat}\n" | nc ${HOST} ${PORT}
 done
